cd3373f41564c9b6dabbf70dd3dca6ffa4d18476
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2023-05-14 22:29:28 -0700

    Thread's ability to return data can be set by the programmer; Added directory class

--- thread.py
+++ thread.py
@@ -1,14 +1,38 @@
 import threading
+import typing
 
 class thread(threading.Thread):
-  def __init__(self, group: None = None, target: None = None, name: str | None = None, args = ..., kwargs = {}) -> None:
+  
+  _return_data = None
+  
+  #==============================================================================#
+  def __init__(self, group: None = None, target: None = None, name: str | None = None, args = ..., kwargs = {}, enable: bool = False) -> None:
     threading.Thread.__init__(self, group, target, name, args, kwargs)
-    self._return = None
+    self._return_data = enable
+    if self._return_data:
+      self._return = None
+  
+  #==============================================================================#
 
-  def run(self):
-    if self._target is not None:
-      self._return = self._target(*self._args, **self._kwargs)
+  #==============================================================================#
+  def run(self) -> None:
+    try:
+      if self._target is not None:
+        if self._return_data:
+          self._return = self._target(*self._args, **self._kwargs)
+        else:
+          self._target(*self._args, **self._kwargs)
+    finally:
+      # Avoid a refcycle if the thread is running a function with
+      # an argument that has a member that points to the thread.
+      del self._target, self._args, self._kwargs
+  
+  #==============================================================================#
 
-  def join(self):
+  #==============================================================================#
+  def join(self) -> typing.Union[any, None]:
     threading.Thread.join(self)
-    return self._return
+    if self._return_data:
+      return self._return
+  
+  #==============================================================================#
